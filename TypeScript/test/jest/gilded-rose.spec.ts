c366565706dbbef452b372a784f3e7b7eb5b028f
faskan <faisalkhanthayub@gmail.com>
2025-02-06 17:53:45 +0100

    Prepare for refactoring with tests not to break working software

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -6,4 +6,17 @@ describe('Gilded Rose', () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe('foo');
   });
+
+  test("normal item degrades in quality", () => {
+    const gildedRose = new GildedRose([new Item("Normal Item", 10, 20)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(19);
+    expect(gildedRose.items[0].sellIn).toBe(9);
+  });
+
+  test("quality never goes below zero", () => {
+    const gildedRose = new GildedRose([new Item("Normal Item", 5, 0)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(0);
+  });
 });
